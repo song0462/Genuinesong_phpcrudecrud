0176a1d5a999c9d52c6150b9d4651f0aec8555d6
song0462 <song0462@umn.edu>
2023-03-27 22:55:45 +0000

    commit

---
+++ credentials.php
@@ -0,0 +1,6 @@
+<?php                            //connection & credentials
+$servername = "localhost";      //mysql is on the same host as apache
+$dbname = "employees";          //which db you're going to use
+$username = "song0462";
+$password = "Mechanicalm18!";
+?>
